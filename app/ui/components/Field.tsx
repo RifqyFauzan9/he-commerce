96d257b0e7e9ffa7ee333587a08dd86598a25d1d
RifqyFauzan9 <r1fqyf4uz4n@gmail.com>
2026-05-06 11:43:15 +0700

    update field komponen to support type props

--- app/ui/components/Field.tsx
+++ app/ui/components/Field.tsx
@@ -1,22 +1,24 @@
 'use client';
 
 import clsx from "clsx";
+import Link from "next/link";
 import { useState } from "react";
 
 type Props = {
     value: string;
     setValue: (value: string) => void;
     label: string;
+    type?: 'text' | 'password';
 }
 
-export default function Field({ value, setValue, label }: Props) {
+export default function Field({ value, setValue, label, type = 'text' }: Props) {
     const [focused, setFocused] = useState<boolean>(false);
 
     return (
-        <div className="flex flex-col gap-1 w-full mb-3">
+        <div className="flex flex-col gap-1 w-full">
             <h4 className="text-sm font-medium">{label}</h4>
             <input
-                type="text"
+                type={type}
                 value={value}
                 onChange={(e) => setValue(e.target.value)}
                 className={clsx("rounded-lg py-2 px-3 text-sm text-muted-foreground outline-none border-1", { "border-primary": focused, "border-border": !focused })}
@@ -24,6 +26,7 @@ export default function Field({ value, setValue, label }: Props) {
                 onBlur={() => setFocused(false)}
                 placeholder={`Masukkan ${label}...`}
             />
+            {type === 'password' && <Link href="#" className="text-primary text-sm text-right mt-1">Lupa password?</Link>}
         </div>
     );
 }
