9297eda3aadd3b1e7a497156b733d00229fc48f4
RifqyFauzan9 <r1fqyf4uz4n@gmail.com>
2026-05-06 13:29:52 +0700

    mengganti color scheme ke hijau dan memperbaiki pewarnaan admin dashboard skeleton komponen

--- app/ui/components/AdminDashboardSkeleton.tsx
+++ app/ui/components/AdminDashboardSkeleton.tsx
@@ -1,22 +1,22 @@
 export default function AdminDashboardSkeleton() {
     return (
         <div className="flex flex-col w-full rounded-lg overflow-hidden border border-border">
-            <div className="w-full bg-secondary flex items-center gap-1 p-3 border-b border-border">
-                <div className="h-2 w-2 bg-muted rounded-full" />
-                <div className="h-2 w-2 bg-muted rounded-full" />
-                <div className="h-2 w-2 bg-muted rounded-full" />
+            <div className="w-full bg-muted flex items-center gap-2 p-3 border-b border-border">
+                <div className="h-2 w-2 bg-border rounded-full" />
+                <div className="h-2 w-2 bg-border rounded-full" />
+                <div className="h-2 w-2 bg-border rounded-full" />
             </div>
 
             <div className="p-3 flex flex-col gap-3 bg-white">
-                <div className="w-30 h-2 md:h-3 rounded-lg bg-secondary" />
+                <div className="w-30 h-2 md:h-4 rounded-md bg-muted" />
 
                 <div className="flex items-center gap-2 w-full">
-                    <div className="flex-1 h-10 md:h-20 bg-secondary rounded-md" />
-                    <div className="flex-1 h-10 md:h-20 bg-secondary rounded-md" />
-                    <div className="flex-1 h-10 md:h-20 bg-secondary rounded-md" />
+                    <div className="flex-1 h-10 md:h-20 bg-muted rounded-md" />
+                    <div className="flex-1 h-10 md:h-20 bg-muted rounded-md" />
+                    <div className="flex-1 h-10 md:h-20 bg-muted rounded-md" />
                 </div>
 
-                <div className="w-full h-30 bg-secondary rounded-md hidden md:block" />
+                <div className="w-full h-30 bg-muted rounded-md hidden md:block" />
             </div>
         </div>
     );
