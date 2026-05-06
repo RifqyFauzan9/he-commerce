7c30f8fc81ad4cf67ed351f6637cdedf0cbd09b0
RifqyFauzan9 <r1fqyf4uz4n@gmail.com>
2026-05-06 11:43:47 +0700

    create LoginForm komponen

---
+++ app/ui/components/LoginForm.tsx
@@ -0,0 +1,21 @@
+'use client';
+
+import { useState } from "react";
+import Field from "./Field";
+import Link from "next/link";
+
+export default function LoginForm() {
+    const [email, setEmail] = useState<string>('');
+    const [password, setPassword] = useState<string>('');
+
+    return (
+        <form onSubmit={() => alert("Anda login")} className="flex flex-col gap-4">
+            <Field label="Email" value={email} setValue={setEmail} />
+            <Field label="Password" value={password} setValue={setPassword} type="password" />
+
+            <button type="submit" className="bg-primary py-2 text-primary-foreground rounded-lg mt-3">Masuk</button>
+
+            <p className="text-center text-xs text-muted-foreground mt-8">&copy; 2024 AdminStore. All rights reserved.</p>
+        </form>
+    )
+}
