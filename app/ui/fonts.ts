eb2725cd9ddf221c68e3f03b41e9ec997dd5cc08
RifqyFauzan9 <r1fqyf4uz4n@gmail.com>
2026-05-06 10:46:29 +0700

    Add fonts

---
+++ app/ui/fonts.ts
@@ -0,0 +1,4 @@
+import { DM_Sans, DM_Mono } from "next/font/google";
+
+export const dmSans = DM_Sans({ subsets: ['latin'], weight: ['300', '400', '500', '600', '700', '800'] });
+export const dmMono = DM_Mono({ subsets: ['latin'], weight: ['300', '400', '500'] });
