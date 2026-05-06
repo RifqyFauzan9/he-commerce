7c43ca496668764cc064355a5495d6ed2894e142
RifqyFauzan9 <r1fqyf4uz4n@gmail.com>
2026-05-06 14:36:15 +0700

    add tabbar to admin dashboard layout

--- app/admin/dashboard/layout.tsx
+++ app/admin/dashboard/layout.tsx
@@ -1,8 +1,8 @@
-import { Menu, Leaf, BellDot, CircleUser } from "lucide-react"
+import { Menu, Leaf, BellDot, LayoutDashboard, Box, ShoppingCart, Cog } from "lucide-react"
 
 export default function DashboardLayout({ children }: { children: React.ReactNode }) {
     return (
-        <main>
+        <div>
             <header className="flex items-center justify-between bg-white p-5 border-b border-border">
                 <div className="flex items-center gap-4">
                     <Menu size={26} strokeWidth={2} />
@@ -20,9 +20,26 @@ export default function DashboardLayout({ children }: { children: React.ReactNod
                 </div>
             </header>
 
-            <div className="p-4">
-                {children}
-            </div>
-        </main>
+            {children}
+
+            <footer className="fixed right-0 left-0 bottom-0 h-20 bg-white border-t border-border flex items-stretch">
+                <div className="flex-1 flex flex-col gap-1 items-center justify-center text-primary">
+                    <LayoutDashboard />
+                    <p className="text-sm font-medium">Dashboard</p>
+                </div>
+                <div className="flex-1 flex flex-col gap-1 items-center justify-center text-muted-foreground">
+                    <Box />
+                    <p className="text-sm font-medium">Produk</p>
+                </div>
+                <div className="flex-1 flex flex-col gap-1 items-center justify-center text-muted-foreground">
+                    <ShoppingCart />
+                    <p className="text-sm font-medium">Pesanan</p>
+                </div>
+                <div className="flex-1 flex flex-col gap-1 items-center justify-center text-muted-foreground">
+                    <Cog />
+                    <p className="text-sm font-medium">Settings</p>
+                </div>
+            </footer>
+        </div>
     )
 }
