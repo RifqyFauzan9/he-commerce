c0c7f109d0f31e14ba39bc53c845ec52d6dd51c4
RifqyFauzan9 <r1fqyf4uz4n@gmail.com>
2026-05-06 14:48:43 +0700

    membuat file placeholder-data dan mengisi dengan beberapa data orders

---
+++ app/lib/placeholder-data.ts
@@ -0,0 +1,20 @@
+export const orders = [
+    {
+        id: '#ORD-001',
+        status: 'paid',
+        buyerName: 'Siti Aminah',
+        price: 350000
+    },
+    {
+        id: '#ORD-002',
+        status: 'waiting',
+        buyerName: 'Budi Santoso',
+        price: 120000
+    },
+    {
+        id: '#ORD-003',
+        status: 'paid',
+        buyerName: 'Rina Melawati',
+        price: 750000
+    },
+]
